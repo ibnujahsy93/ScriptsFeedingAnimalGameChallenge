89aeb00c9731cae18ec1ca98d3fa93c1491ca179
ibnujahsy93 <55024099+ibnujahsy93@users.noreply.github.com>
2022-04-11 09:50:38 +0700

    Initial commit

---
+++ PlayerControllerX.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+    public float cooldownTimer = 2.0f;
+    private bool cooldown = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // On spacebar press, send dog
+        if (Input.GetKeyDown(KeyCode.Space) && cooldown == false)
+        {
+            Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+            Invoke("ResetCooldown", cooldownTimer);
+            cooldown = true;
+        }
+    }
+
+    //Add a new method
+    void ResetCooldown()
+    {
+        cooldown = false;
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# ScriptsFeedingAnimalGameChallenge
+ ScriptsFeedingAnimalGameChallenge
